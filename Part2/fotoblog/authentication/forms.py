e31e0143010c772b66a1112e8d6ea072dd4b0f2c
Fabrice <112811649+Derfro22@users.noreply.github.com>
2024-02-14 16:00:54 +0100

    login and logout with password reset totally OK

--- Part2/fotoblog/authentication/forms.py
+++ Part2/fotoblog/authentication/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d\'utilisateur')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+
+
+class UserProfileForm(forms.Form):
+    # Définissez les champs de votre formulaire ici
+    pass
