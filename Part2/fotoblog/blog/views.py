f3dae1d925b9e7c0d10af0ddfa5e3b8d2b5e585f
Fabrice <112811649+Derfro22@users.noreply.github.com>
2024-02-22 17:15:34 +0100

    adding photo is possible update and delete too

--- Part2/fotoblog/blog/views.py
+++ Part2/fotoblog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 from . import forms
 from . import models
@@ -7,7 +8,8 @@ from . import models
 @login_required
 def home(request):
     photos = models.Photo.objects.all()
-    return render(request, 'blog/home.html', context={'photos': photos})
+    blogs = models.Blog.objects.all()
+    return render(request, 'blog/home.html', context={'photos': photos, 'blogs': blogs})
 
 @login_required
 def photo_upload(request):
@@ -22,3 +24,53 @@ def photo_upload(request):
             photo.save()
             return redirect('home')
     return render(request, 'blog/photo_upload.html', context={'form': form})
+
+@login_required
+def blog_and_photo_upload(request):
+    blog_form = forms.BlogForm()
+    photo_form = forms.PhotoForm()
+
+    if request.method == 'POST':
+        blog_form = forms.BlogForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        if all([blog_form.is_valid(), photo_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.photo = photo
+            blog.save()
+            return redirect('home')
+    context = {
+        'blog_form': blog_form,
+        'photo_form': photo_form,
+    }
+    return render(request, 'blog/create_blog_post.html', context=context)
+
+@login_required	
+def view_blog(request, blog_id):
+    blog = get_object_or_404(models.Blog, id=blog_id)
+    return render(request, 'blog/view_blog.html', {'blog': blog})
+
+@login_required
+def edit_blog(request, blog_id):
+    blog = get_object_or_404(models.Blog, id=blog_id)
+    edit_form = forms.BlogForm(instance=blog)
+    delete_form = forms.DeleteBlogForm()
+    if request.method == 'POST':
+        if 'edit_blog' in request.POST:
+            edit_form = forms.BlogForm(request.POST, instance=blog)
+            if edit_form.is_valid():
+                edit_form.save()
+                return redirect('home')
+            if 'delete_blog' in request.POST:
+                delete_form = forms.DeleteBlogForm(request.POST)
+                if delete_form.is_valid():
+                    blog.delete()
+                    return redirect('home')
+    context = {
+        'edit_form': edit_form,
+        'delete_form': delete_form,
+    }
+    return render(request, 'blog/edit_blog.html', context=context)
--- Part2/fotoblog/fotoblog/urls.py
+++ Part2/fotoblog/fotoblog/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('home/', blog.views.home, name='home'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
     path('profile-photo/upload/', authentication.views.upload_profile_photo, name='upload_profile_photo'),
+    path('blog/create', blog.views.blog_and_photo_upload, name='blog_create'),
+    path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
+    path('blog/<int:blog_id>/edit', blog.views.edit_blog, name='edit_blog'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
